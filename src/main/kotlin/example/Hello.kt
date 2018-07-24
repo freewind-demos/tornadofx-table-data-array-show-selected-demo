17b9d75a2ac1df7dd1a0f5e34d2d4e76c2403bf1
freewind <nowindlee@gmail.com>
2018-07-24 14:18:12 +0800

    table

--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -6,12 +6,19 @@ import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 
-class User(id: Int, name: String) {
-    val idProperty = SimpleIntegerProperty(id)
-    var id by idProperty
+// This kind of definition seems have issue
+// <https://stackoverflow.com/questions/51491615/change-the-value-of-a-property-of-some-data-but-tableview-doesnt-update>
+//class User(id: Int, name: String) {
+//    val idProperty = SimpleIntegerProperty(id)
+//    var id by idProperty
+//
+//    val nameProperty = SimpleStringProperty(name)
+//    var name by nameProperty
+//}
 
-    val nameProperty = SimpleStringProperty(name)
-    var name by nameProperty
+class User(id: Int, name: String) {
+    val id = SimpleIntegerProperty(id)
+    val name = SimpleStringProperty(name)
 }
 
 private val data = listOf(User(111, "AAA"), User(222, "BBB"), User(333, "CCC"), User(444, "DDD")).observable()
@@ -20,10 +27,11 @@ class HelloWorld : View() {
 
     override val root = vbox {
         tableview(data) {
-            column("id", User::id)
-            column("name", User::name)
+            column("id", User::id).minWidth(80)
+            column("name", User::name).minWidth(200)
         }
     }
+
 }
 
 class HelloWorldStyle : Stylesheet() {
